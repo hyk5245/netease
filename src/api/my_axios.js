c1b60c69f7f3377aa43b1a1571fe358ca3585705
黄英康 <1520018309@qq.com>
2022-04-01 15:57:17 +0800

    修改了服务器获取数据地址

--- src/api/my_axios.js
+++ src/api/my_axios.js
@@ -3,17 +3,17 @@ import nprogress from 'nprogress'
 import "nprogress/nprogress.css"
 
 let requests = axios.create({
-  // baseURL: 'https://netease-cloud-api-pink.vercel.app',
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://netease-cloud-api-pink.vercel.app',
+  // baseURL: 'http://localhost:3000',
   timeout: 7000
 })
 
 requests.interceptors.request.use((config) => {
-  // if (config.url.indexOf('?') !== -1)
-  //   config.url += '&realIP=10.33.38.171';   // 因为部署在vercel上，所以添加这一个参数
-  // else {
-  //   config.url += '?realIP=10.33.38.171';
-  // }
+  if (config.url.indexOf('?') !== -1)
+    config.url += '&realIP=10.33.38.171';   // 因为部署在vercel上，所以添加这一个参数
+  else {
+    config.url += '?realIP=10.33.38.171';
+  }
   nprogress.start();
   return config;
 })
